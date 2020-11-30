cbde460a5fc939350c143cea9002e7db788bf8dc
Friderike <72058743+Friderike@users.noreply.github.com>
2020-12-01 00:02:03 +0100

    ?

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,21 @@
 console.log(`Hi there, thanks for stopping by!`)
+
+
+/*const navSlide = ()=> {
+
+    const hamburger = $(`.hamburger`);
+   const navS = $(`.slide`); 
+   // $ is equal to document.querySelector or document.querySelectorAll
+   
+
+   hamburger.addEventListener(`click`, () => {
+       navS.classlist.toggle(`slide-active`);
+   }); WHY IS THIS NOT A FUNCTION?
+
+   $(`.hamburger`).on(`click`, function() {
+       navS.classlist.toggle(`slide-active`);
+   })
+} 
+
+navSlide();*/
+
